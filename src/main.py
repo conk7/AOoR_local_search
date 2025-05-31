34b8a9049e614cc0bf47802529982bc06d988d50
Vladislav Tyurin <super.g5433@yandex.ru>
2025-05-31 20:21:41 +0300

    update

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from utils import measure_algo, print_results, BENCHMARKS_PATH, ANSWERS_PATH, N
 
 
 def main():
-    for algo in [iterated_local_search]:
+    for algo in [best_improvement_local_search, iterated_local_search]:
         benchmarks, avg_times, total_perms, total_cost = measure_algo(
             algo, BENCHMARKS_PATH, N
         )
